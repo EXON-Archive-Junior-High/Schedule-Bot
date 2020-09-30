cd81e73334e4defaadcb209561edd59be0a98596
1-exon <exon.indiv@gmail.com>
2020-09-30 15:32:55 +0900

    push

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const client = new Client()
 const prefix = '!'
 
 const schedule = [
-    {"subject":"월요일"},
-    {"subject":"화요일"},
-    {"subject":"수요일"},
-    {"subject":"목요일"},
-    {"subject":"금요일"},
+    {'day':'월요일', "subject":[{'title':"월요일", 'description':'아주 뭐같은 월요일'}, {'title':"월요일", 'description':'아주 뭐같은 월요일'}, {'title':"월요일", 'description':'아주 뭐같은 월요일'}]},
+    {'day':'화요일', "subject":[{'title':"화요일", 'description':'아주 뭐같은 화요일'}, {'title':"월요일", 'description':'아주 뭐같은 월요일'}, {'title':"월요일", 'description':'아주 뭐같은 월요일'}]},
+    {'day':'수요일', "subject":[{'title':"수요일", 'description':'아주 뭐같은 수요일'}, {'title':"수요일", 'description':'아주 뭐같은 수요일'}, {'title':"수요일", 'description':'아주 뭐같은 수요일'}]},
+    {'day':'목요일', "subject":[{'title':"목요일", 'description':'아주 뭐같은 목요일'}, {'title':"목요일", 'description':'아주 뭐같은 목요일'}, {'title':"목요일", 'description':'아주 뭐같은 목요일'}]},
+    {'day':'금요일', "subject":[{'title':"금요일", 'description':'아주 뭐같은 금요일'}, {'title':"금요일", 'description':'아주 뭐같은 금요일'}, {'title':"금요일", 'description':'아주 뭐같은 금요일'}]},
 ]
 
 client.on('ready', () => console.log('[System] Start'))
@@ -17,18 +17,15 @@ client.on('message', (msg) => {
     if (msg.author.bot) return
     if (msg.content === `${prefix}시간표`) {
       const day = new Date().getDay()
-      msg.channel.send(schedule[day - 1].subject || "응 오늘 학교 안가~~")
+      let embed = new MessageEmbed({title:`${schedule[day - 1].day} 시간표`})
+      schedule[day - 1].subject.forEach((i) => {
+        embed.addField(i.title, i.description)
+      })
+      msg.channel.send(embed)
     }
-    client.login(setting.token)
+
 })
 
-while (true) {
-    const date = Date()
-    if (date.getHours() === 22 && date.getMinutes() === 23 && date.getSeconds() === 0) {
-        console.log('ㅇ')
-    }
-}
 
-
-
+client.login(setting.token)
 
